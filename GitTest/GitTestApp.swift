569b7141de4b88d5a75e713fb25cf82f75ae3299
HessahAlkhzaimi <h00460215@hct.ac.ae>
2025-01-26 13:23:43 +0400

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Hessah  on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitTestTests/GitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestTests.swift
+//  GitTestTests
+//
+//  Created by Hessah  on 26/01/2025.
+//
+
+import Testing
+@testable import GitTest
+
+struct GitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
